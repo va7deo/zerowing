b7200489c45382647e3a27b3c09f5ead574c466c
birdybro <aberusugi@gmail.com>
2022-01-22 11:22:51 -0700

    update sys, move sources, start fresh 17.0 pll's
    preparing for full update and fixing the dual version conflicts with clocks

--- rtl/pll/pll_0002.v
+++ rtl/pll/pll_0002.v
@@ -10,9 +10,6 @@ module  pll_0002(
 	// interface 'outclk0'
 	output wire outclk_0,
 
-	// interface 'outclk1'
-	output wire outclk_1,
-
 	// interface 'locked'
 	output wire locked
 );
@@ -21,12 +18,12 @@ module  pll_0002(
 		.fractional_vco_multiplier("false"),
 		.reference_clock_frequency("50.0 MHz"),
 		.operation_mode("direct"),
-		.number_of_clocks(2),
-		.output_clock_frequency0("70.000000 MHz"),
+		.number_of_clocks(1),
+		.output_clock_frequency0("20.000000 MHz"),
 		.phase_shift0("0 ps"),
 		.duty_cycle0(50),
-		.output_clock_frequency1("70.000000 MHz"),
-		.phase_shift1("-2500 ps"),
+		.output_clock_frequency1("0 MHz"),
+		.phase_shift1("0 ps"),
 		.duty_cycle1(50),
 		.output_clock_frequency2("0 MHz"),
 		.phase_shift2("0 ps"),
@@ -80,7 +77,7 @@ module  pll_0002(
 		.pll_subtype("General")
 	) altera_pll_i (
 		.rst	(rst),
-		.outclk	({outclk_1, outclk_0}),
+		.outclk	({outclk_0}),
 		.locked	(locked),
 		.fboutclk	( ),
 		.fbclk	(1'b0),
